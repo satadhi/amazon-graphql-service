c902f933abf5e733e4145d8e5f7838e10bb36ed8
satadhi <haldersatadhi@gmail.com>
2024-08-20 13:54:11 +0530

    intial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) { }
+
+  async create(createUserInput: CreateUserInput) {
+    return this.usersRepository.create({
+      ...createUserInput,
+      password: await this.hashPassword(createUserInput.password),
+    });
+  }
+
+  private async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+
+
+  async findOne(_id: string) {
+    return this.usersRepository.findOne({ _id });
+  }
+
+  async update(_id: string, updateUserInput: UpdateUserInput) {
+    return this.usersRepository.findOneAndUpdate(
+      { _id },
+      {
+        $set: {
+          ...updateUserInput,
+          password: await this.hashPassword(updateUserInput.password),
+        },
+      },
+    );
+  }
+
+  async remove(_id: string) {
+    return this.usersRepository.findOneAndDelete({ _id });
+  }
+}
